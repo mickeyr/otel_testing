d545a38df1d2224e7cbde0419c3feca064f36733
Malcolm Roberts <malcolm@careevolution.com>
2025-02-17 15:09:18 -0600

    A few dotnet fixes

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,83 +1,173 @@
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
 using System.Globalization;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
-using OpenTelemetry;
-using OpenTelemetry.Exporter.OpenTelemetryProtocol;
-using OpenTelemetry.Logs;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
+using Microsoft.Net.Http.Headers;
+using Types;
+using dotnet;
 
 var builder = WebApplication.CreateBuilder(args);
-const string serviceName = "rolldice";
-var rollMeter = new Meter("dotnet-api", "1.0.0");
-var countRolls = rollMeter.CreateCounter<long>("rolls");
-var anonymousRolls = rollMeter.CreateCounter<long>("anonymous_rolls");
-var namedRolls = rollMeter.CreateCounter<long>("named_rolls");
-var rollActivitySource = new ActivitySource("dotnet-api");
 
+var configuration = builder.Configuration;
 
-var otel = builder
-  .Services
-  .AddOpenTelemetry();
-
-otel.ConfigureResource(r => r
-      .AddService(
-        serviceName: serviceName,
-        serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
-        serviceInstanceId: Environment.MachineName));
-otel.WithTracing(tracing => tracing
-      .AddSource("TestApp.AspNetCore")
-      .SetSampler(new AlwaysOnSampler())
-      .AddHttpClientInstrumentation()
-      .AddAspNetCoreInstrumentation()
-      .AddSource(rollActivitySource.Name)
-    );
-otel.WithMetrics(metrics => metrics
-      .AddMeter("TestApp.AspNetCore", "1.0.0")
-      .SetExemplarFilter(ExemplarFilterType.TraceBased)
-      .AddMeter(rollMeter.Name)
-      .AddMeter("Microsoft.AspNetCore.Hosting")
-      .AddMeter("Microsoft.AspNetCore.Http")
-      .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
-      .AddHttpClientInstrumentation()
-      .AddAspNetCoreInstrumentation()
-  );
-otel.WithLogging(logging => logging
-        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName)));
-
-var OtlpEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
-if (!string.IsNullOrWhiteSpace(OtlpEndpoint))
-{
-  otel.UseOtlpExporter();
-}
+TelemetryExtensions.UseTelemetry(builder);
 
 var app = builder.Build();
 
+Meter meter = new("AircraftLookup");
+Counter<int> aircraftLookupCounter = meter.CreateCounter<int>("AircraftLookupCounter");
+Histogram<double> aircraftLookupDuration = meter.CreateHistogram<double>("AircraftLookupDuration");
+Counter<int> aircraftLookupStatusCodeCounter = meter.CreateCounter<int>("AircraftLookupStatusCodeCounter");
+Counter<int> aircraftLookupErrorCounter = meter.CreateCounter<int>("AircraftLookupErrorCounter");
+Counter<int> aircraftLookupSuccessCounter = meter.CreateCounter<int>("AircraftLookupSuccessCounter");
+Counter<long> aircraftLookupPayloadSizeCounter = meter.CreateCounter<long>("AircraftLookupPayloadSizeCounter");
 
-string HandleRollDice([FromServices] ILogger<Program> logger, string? player)
+Counter<int> airportLookupCounter = meter.CreateCounter<int>("AirportLookupCounter");
+Histogram<double> airportLookupDuration = meter.CreateHistogram<double>("AirportLookupDuration");
+Counter<int> airportLookupStatusCodeCounter = meter.CreateCounter<int>("AirportLookupStatusCodeCounter");
+Counter<int> airportLookupErrorCounter = meter.CreateCounter<int>("AirportLookupErrorCounter");
+Counter<int> airportLookupSuccessCounter = meter.CreateCounter<int>("AirportLookupSuccessCounter");
+Counter<long> airportLookupPayloadSizeCounter = meter.CreateCounter<long>("AirportLookupPayloadSizeCounter");
+
+ActivitySource activitySource = new("AircraftLookup");
+
+async Task<Aircraft> GetAircraft(string registration, ILogger<Program> logger)
 {
-  using var activity = rollActivitySource.StartActivity("RollDice");
-  countRolls.Add(1);
-  var result = RollDice();
+  using var activity = activitySource.StartActivity("GetAircraft");
+  logger.LogInformation("Getting aircraft info for {Registration}", registration);
+  aircraftLookupCounter.Add(1, new KeyValuePair<string, object?>("registration", registration));
 
-  if (string.IsNullOrWhiteSpace(player))
+  using var client = new HttpClient();
+  client.BaseAddress = new Uri("https://api.adsbdb.com/v0/");
+  client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+  var route = $"aircraft/{registration}";
+  var timer = Stopwatch.StartNew();
+  var response = await client.GetAsync(route);
+  timer.Stop();
+  aircraftLookupDuration.Record(timer.Elapsed.TotalMilliseconds, new KeyValuePair<string, object?>("registration", registration));
+  aircraftLookupStatusCodeCounter.Add(1,
+      new KeyValuePair<string, object?>("registration", registration),
+      new KeyValuePair<string, object?>("statusCode", (int)response.StatusCode)
+    );
+  if (!response.IsSuccessStatusCode)
   {
-    anonymousRolls.Add(1);
-    logger.LogInformation("Anonymous player is rolling the dice: {result}", result);
-  }
-  else
-  {
-    namedRolls.Add(1);
-    logger.LogInformation("{player} is rolling the dice: {result}", player, result);
+    aircraftLookupErrorCounter.Add(1, new KeyValuePair<string, object?>("registration", registration));
+    logger.LogError("Failed to get aircraft info: {route}", route);
+    throw new Exception("Failed to get aircraft info");
   }
 
-  return result.ToString(CultureInfo.InvariantCulture);
+  aircraftLookupSuccessCounter.Add(1, new KeyValuePair<string, object?>("registration", registration));
+  aircraftLookupPayloadSizeCounter.Add(response.Content.Headers.ContentLength ?? 0, new KeyValuePair<string, object?>("registration", registration));
+
+  var payload = await response.Content.ReadFromJsonAsync<AircraftResponsePayload>();
+  if (payload?.Response?.Aircraft == null)
+  {
+    logger.LogError("Failed to get aircraft info");
+    throw new Exception("Failed to get aircraft info");
+  }
+
+
+  return payload.Response.Aircraft;
 }
 
-int RollDice() => Random.Shared.Next(1, 7);
+async Task<List<string>> GetRegistrationCodesForAiport(string airportCode, DateTimeOffset begin, DateTimeOffset end, ILogger<Program> logger)
+{
+  using var activity = activitySource.StartActivity("GetRegistrationCodesForAiport");
+  logger.LogInformation("Getting registrations for airport {AirportCode}", airportCode);
+  airportLookupCounter.Add(1, new KeyValuePair<string, object?>("airportCode", airportCode));
 
-app.MapGet("/rolldice/{player?}", HandleRollDice);
+  using var client = new HttpClient();
+  var arrivalsUrl = configuration.GetValue<string>("ArrivalsUrl");
+  if (string.IsNullOrWhiteSpace(arrivalsUrl))
+  {
+    airportLookupErrorCounter.Add(1, new KeyValuePair<string, object?>("airportCode", airportCode));
+    throw new Exception("ArrivalsUrl is not configured");
+  }
+  client.BaseAddress = new Uri(arrivalsUrl);
+  var timer = Stopwatch.StartNew();
+  var response = await client.GetAsync($"arrivals?airport={airportCode}&begin={begin.ToUnixTimeSeconds()}&end={end.ToUnixTimeSeconds()}");
+  timer.Stop();
+  airportLookupDuration.Record(timer.Elapsed.TotalMilliseconds, new KeyValuePair<string, object?>("airportCode", airportCode));
+  airportLookupPayloadSizeCounter.Add(response.Content.Headers.ContentLength ?? 0, new KeyValuePair<string, object?>("airportCode", airportCode));
+  airportLookupStatusCodeCounter.Add(1,
+      new KeyValuePair<string, object?>("airportCode", airportCode),
+      new KeyValuePair<string, object?>("statusCode", (int)response.StatusCode)
+    );
+  if (!response.IsSuccessStatusCode)
+  {
+    airportLookupErrorCounter.Add(1, new KeyValuePair<string, object?>("airportCode", airportCode));
+    throw new Exception("Failed to get arrivals");
+  }
+
+  airportLookupSuccessCounter.Add(1, new KeyValuePair<string, object?>("airportCode", airportCode));
+  var aircraft = await response.Content.ReadFromJsonAsync<List<Dictionary<string, string>>>() ?? [];
+  var registrations = aircraft.Select(a => a["icao24"]).ToList();
+
+  return registrations;
+}
+
+async Task<List<Aircraft>> HandleGetPlaneInfo([FromServices] ILogger<Program> logger, string airportCode = "KBNA")
+{
+  using var activity = activitySource.StartActivity("HandleGetPlaneInfo");
+  logger.LogInformation("Getting plane info for airport {AirportCode}", airportCode);
+
+  var registrations = await GetRegistrationCodesForAiport(airportCode, DateTime.UtcNow.AddDays(-1), DateTime.UtcNow, logger);
+  var planes = new List<Aircraft>();
+  foreach (var registration in registrations)
+  {
+    try
+    {
+      var aircraft = await GetAircraft(registration, logger);
+      planes.Add(aircraft);
+    }
+    catch (Exception ex)
+    {
+      logger.LogError(ex, "Failed to get aircraft info for {Registration}", registration);
+    }
+  }
+
+  return planes;
+}
+
+app.MapGet("/planes", HandleGetPlaneInfo);
 
 app.Run();
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace Types
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+{
+  public record AircraftResponsePayload(
+      AircraftResponse Response
+      )
+  { }
+
+  public record AircraftResponse(
+      Aircraft Aircraft
+  )
+  { }
+
+  public record Aircraft(
+      string Type,
+      [property: JsonPropertyName("icao_type")]
+      string IcaoType,
+      string Manufacturer,
+      [property: JsonPropertyName("mode_s")]
+      string Mode,
+      string Registration,
+      [property: JsonPropertyName("registered_owner_country_iso_name")]
+      string RegisteredOwnerCountryIsoName,
+      [property: JsonPropertyName("registered_owner_country_name")]
+      string RegisteredOwnerCountryName,
+      [property: JsonPropertyName("registered_owner_operator_flag_code")]
+      string RegisteredOwnerOperatorFlagClode,
+      [property: JsonPropertyName("registered_owner")]
+      string RegisteredOwner,
+      [property: JsonPropertyName("url_photo")]
+      string UrlPhoto,
+      [property: JsonPropertyName("url_photo_thumbnail")]
+      string UrlPhotoThumbnail)
+  { }
+}
+
---
+++ dotnet/Telemetry.cs
@@ -0,0 +1,51 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Exporter.OpenTelemetryProtocol;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace dotnet;
+
+internal static class TelemetryExtensions
+{
+  const string serviceName = "AircraftLookup";
+  internal static void UseTelemetry(this WebApplicationBuilder builder)
+  {
+    var otel = builder
+      .Services
+      .AddOpenTelemetry();
+
+    otel.ConfigureResource(r => r
+          .AddService(
+            serviceName: serviceName,
+            serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+            serviceInstanceId: Environment.MachineName));
+    otel.WithTracing(tracing => tracing
+          .AddSource("TestApp.AspNetCore")
+          .AddSource("AircraftLookup")
+          .SetSampler(new AlwaysOnSampler())
+          .AddHttpClientInstrumentation()
+          .AddAspNetCoreInstrumentation()
+        );
+    otel.WithMetrics(metrics => metrics
+          .AddMeter("TestApp.AspNetCore", "1.0.0")
+          .SetExemplarFilter(ExemplarFilterType.TraceBased)
+          .AddMeter("Microsoft.AspNetCore.Hosting")
+          .AddMeter("Microsoft.AspNetCore.Http")
+          .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+          .AddMeter("AircraftLookup")
+          .AddHttpClientInstrumentation()
+          .AddAspNetCoreInstrumentation()
+      );
+    otel.WithLogging(logging => logging
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName)));
+
+    var OtlpEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+    if (!string.IsNullOrWhiteSpace(OtlpEndpoint))
+    {
+      otel.UseOtlpExporter();
+    }
+  }
+
+}
